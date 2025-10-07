bb17d454b511d0ec2342c7482340722f7f1ebf1a
박희선 <heesun@heesunp.wifi.local.cmu.edu>
2025-10-07 10:08:25 -0400

    name to heesunp

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -8,7 +8,7 @@ export default function QueryProcessor(query: string): string {
   }
 
   if (query.toLowerCase().includes("name")) {
-    return "ami";
+    return "heesunp";
   }
 
   if (query.toLowerCase().includes("andrew id")) {
